3e8c527d0cd352d8d0b72b7e936b9995f982767b
Muzammil Khan <muzammil.khan@outlook.in>
2021-12-27 16:28:53 +0530

    client init and auth

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+
+function App() {
+	useEffect(() => {
+		const params = new URLSearchParams(window.location.search)
+		const accessToken = params.get("access_token")
+		const refreshToken = params.get("refresh_token")
+		console.log(accessToken, refreshToken)
+	}, [])
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a className="App-link" href="http://localhost:3001/login">
+					Login with Spotify
+				</a>
+			</header>
+		</div>
+	)
+}
+
+export default App
